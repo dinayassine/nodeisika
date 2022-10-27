fdc9aebb9aa8a8a2a8a06a6dd0c3100a826ecf62
dinayassine <44085778+dinayassine@users.noreply.github.com>
2022-10-27 13:08:06 +0300

    Update calculator.test.js

--- calculator.test.js
+++ calculator.test.js
@@ -1,5 +1,5 @@
 const calculator = require('./calculator')
  
 test('string with a single number should result in the number itself', () => {
-    expect(calculator.add(3,4)).toBe(7);
+    expect(calculator.add(3,4)).toBe(9);
   });
